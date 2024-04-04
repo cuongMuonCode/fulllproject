01839105e0bf61af459b0ed27587b191b68c23b4
cuongMuonCode <cuongdepzai151024@gmail.com>
2024-04-04 19:46:21 +0700

    classRoom
    add classRoom

--- src/main/java/com/example/LTNC_WEB_1/classRoom/classRoomService.java
+++ src/main/java/com/example/LTNC_WEB_1/classRoom/classRoomService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.StandardBeanInfoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,5 +18,15 @@ public class classRoomService {
 
     public classRoom getClassAndCourseId(String courseId,String classId){
         return classRoomRepository.findClassRoomByClassIdAndCourseId(classId,courseId);
+    }
+
+    public classRoom createClassRoom(String classId,String courseId,Integer day, Integer shift){
+        if(classRoomRepository.findClassRoomByClassIdAndCourseId(classId,courseId)!=null) {
+            // thong bao ton tai lop hoc nay
+            System.out.println("da ton tai lop hoc nay");
+            return null ;
+        }
+        classRoom newClass = new classRoom(classId,courseId,new ArrayList<>(),day, shift,50);
+        return classRoomRepository.save(newClass);
     }
 }
